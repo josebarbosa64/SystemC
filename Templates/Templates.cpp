07ba3aa278c3e7d976abde81ff0df643a6680a86
josebarbosa64 <159543525+josebarbosa64@users.noreply.github.com>
2024-03-05 14:31:45 +0100

    Update Templates.cpp

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -19,4 +19,41 @@ void process()
 }
 };
 
+//RS LATCH with and
+SC_MODULE (nand)
+{
+    sc_in<bool> a;
+    sc_in<bool> b;
+    sc_out<bool> c;
+    SC_CTOR (nand):a("a"),b("b"),c("c")
+    {
+      SC_METHOD (process);
+      sensitive << a << b;
+    }
+    void process()
+    {
+      c.write(!(a.read() && b.read()));
+    }
+};
+
+
+SC_MODULE (rslatch2)
+{
+    sc_in<bool> S;
+    sc_in<bool> R;
+    sc_out<bool> N;
+    sc_out<bool> Q;
+    nand n1, n2;
+    SC_CTOR (rslatch2):S("S"),R("R"),N("N"),Q("Q"), n1("n1"),n2("n2")
+    {
+        n1.a.bind(S);
+        n1.b.bind(Q);
+        n1.c.bind(N);
+
+        n2.a.bind(N);
+        n2.b.bind(R);
+        n2.c.bind(Q);
+    }
+};
+
 
