2b322bd62af9866037c45effc1255f04f48af8bf
Jose <josebarbosa-64@hotmail.com>
2024-03-11 11:27:54 +0100

    exercise 5 corrected

--- Exercise5/kpn.cpp
+++ Exercise5/kpn.cpp
@@ -6,28 +6,58 @@
 kpn::kpn(const sc_module_name &name): sc_module(name), a(10), b(10), c(10), d(10){
     b.write(1);
     c.write(0);
-    SC_THREAD(process);
+    SC_THREAD(split);
+    sensitive<<b.data_written_event()<<b.data_read_event();
+    
+    SC_THREAD(add);
+    sensitive<<c.data_written_event()<<c.data_read_event()<<a.data_written_event()<<a.data_read_event();
+    dont_initialize();          
+    SC_THREAD(delay);
+    sensitive<<d.data_written_event()<<d.data_read_event();
+    dont_initialize();          
+    
+    
+    
+    
 }
 
-void kpn::process(){
-    for (int i=0; i < 10; i++){
-        split();
-    }
-    cout << "Is a fibonacci serie :D" << endl;
-    sc_stop();
-}
 void kpn::split(){
-    unsigned int temp;
-    temp = b.read();
-    a.write(temp);
-    d.write(temp);
-    cout << temp << endl;
-    add();
+    while(true)
+    {
+        
+        for (int i=0; i < 10; i++)
+        {
+            unsigned int temp;
+            temp = b.read();
+            a.write(temp);
+            d.write(temp);
+            cout << temp << endl;
+            //cout<<"iteration "<<i<<endl;
+        }
+        cout << "Is a fibonacci serie :D" << endl;
+        sc_stop();
+        wait();
+
+    }
 }
-void kpn::add(){
-    b.write(a.read() + c.read());
-    delay();
+
+void kpn::add()
+{
+    while(true)
+    {
+        wait();
+        
+        b.write(a.read() + c.read());
+    }
+  
+  
 }
 void kpn::delay(){
-    c.write(d.read());
+    while(true)
+
+    {
+        wait();
+        c.write(d.read());
+    }
+   
 }
